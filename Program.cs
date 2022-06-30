cacb11ebfdecde4bb5dc6f9421be2641949c5e89
Stefano Calzoni <stefanoclz@hotmail.it>
2022-06-30 17:46:15 +0200

    Aggiunto controllo presenza libri e dvd

--- Program.cs
+++ Program.cs
@@ -32,3 +32,41 @@ if(scelta1 == 1)
         Console.WriteLine("Utente non trovato");
     }
 }
+
+Console.WriteLine("Accesso effettuato, cosa vuoi fare?");
+Console.WriteLine("1. Cerca Libro");
+Console.WriteLine("2. Cerca Dvd");
+int scelta2 = Int32.Parse(Console.ReadLine());
+
+if(scelta2 == 1)
+{
+    Console.WriteLine("Inserisci il titolo o l'autore del libro da cercare:");
+    string cercaLibro = Console.ReadLine();
+
+    bool check = new Liste().CheckBook(cercaLibro);
+
+    if(check == true)
+    {
+        Console.WriteLine("Libro trovato!");
+    } else
+    {
+        Console.WriteLine("Spiacenti, libro non presente");
+    }
+
+} else if( scelta2 == 2)
+{
+    Console.WriteLine("Inserisci il titolo o il regista del dvd da cercare:");
+    string cercaDvd = Console.ReadLine();
+
+    bool check = new Liste().CheckDvd(cercaDvd);
+
+    if (check == true)
+    {
+        Console.WriteLine("Dvd trovato!");
+    } else
+    {
+        Console.WriteLine("Spiacenti, dvd non trovato");
+    }
+}
+
+
