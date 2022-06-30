fefc086407ce48183489f27d8c5601e05074d7ea
Stefano Calzoni <stefanoclz@hotmail.it>
2022-06-30 14:50:43 +0200

    inizializzata creazione utente

---
+++ Biblioteca.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Biblioteca
+    {
+
+        public Biblioteca()
+        {
+            Console.WriteLine("inserisci nome");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("inserisci cognome");
+            string cognome = Console.ReadLine();
+
+            Console.WriteLine("inserisci password");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("inserisci email");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("inserisci telefono");
+            string telefono = Console.ReadLine();
+
+            User utente = new User(nome, cognome, email, password, telefono);
+
+            if(utente.isLoggedIn == true)
+            {
+                Console.WriteLine("Utente registrato con successo!");
+            } else
+            {
+                Console.WriteLine("Dati inseriti errati");
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class User
+    {
+        public string name = "";
+        public string surname = "";
+        public string email = "";
+        public string password = "";
+        public string phone;
+
+        public bool isLoggedIn = false;
+
+
+        public User(string name, string surname, string email, string password, string phone)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.email = email;
+            this.password = password;
+            this.phone = phone;
+
+            this.isLoggedIn = true;
+        }
+    }
+}
